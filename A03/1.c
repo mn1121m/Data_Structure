69d1c8d41d1cce660a174abf3bff0f655b2324cd
mn1121m <jymoon1121@gmail.com>
2021-06-11 22:15:21 +0900

    A03

---
+++ A03/1.c
@@ -0,0 +1,38 @@
+/*
+자료구조응용
+03. 성능측정(Performance Measurement)
+
+1. 다음 프로그램의 func1, func2, func3을 정의한 후 실행되도록 구현하라.
+
+- 질문 - 
++ pNum = func1()  <-- 이 부분에 대해서 어떻게 하는질 모르겠습니다.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int func1();
+
+int main(void)
+{
+    int *pNum;
+
+    pNum = func1(); // int 크기만큼 동적할당 받아 10을 지정후 정수형 포인터 리턴 
+    printf("%d\n", *pNum);
+
+    // func2(pNum);// pNum이 가리키는 것을 100으로 수정 
+    // printf("%d\n", *pNum);
+
+    // func3(&pNum); // pNum이 가리키는 것을 200으로 수정 
+    // printf("%d\n", *pNum);
+
+    free(pNum);
+
+    return 0;
+}
+int func1(int * p)
+{
+    p = (int*)malloc(sizeof(int));
+    *p = 10;
+    return *p;
+}
