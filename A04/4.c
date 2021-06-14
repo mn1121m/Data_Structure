c98c78a3723b4c4cb0bcabfdd6cdd8d65d7cc28c
mn1121m <jymoon1121@gmail.com>
2021-06-14 23:57:23 +0900

    A04

--- A04/4.c
+++ A04/4.c
@@ -4,10 +4,12 @@
 
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define FALSE 0
 #define TRUE 1
+#define MAX_NAME_SIZE 32
 
 typedef struct _humanbeing {
     char name[MAX_NAME_SIZE];
@@ -15,12 +17,61 @@ typedef struct _humanbeing {
     float salary;
 } humanBeing;
 
+int humansEqual(humanBeing person1, humanBeing person2);
+
 int main(void)
 {
     humanBeing person1, person2;
+    int flag;
+    printf("Input person1's name, age, salary :\n");
+    fgets(person1.name, sizeof(person1.name), stdin);
+    scanf("%d", &person1.age);
+    scanf("%f", &person1.salary);
+    getchar();
 
-    printf("Input person1's name, age, salary : \n");
-    
+    printf("Input person2's name, age, salary :\n");
+    fgets(person2.name, sizeof(person2.name), stdin);
+    scanf("%d", &person2.age);
+    scanf("%f", &person2.salary);
 
+    flag = humansEqual(person1, person2);
+
+    if(flag)
+        printf("The to human beings are the same\n");
+    else 
+        printf("The to human beings are not the same\n");
     return 0;
 }
+int humansEqual(humanBeing person1, humanBeing person2)
+{
+    if(strcmp(person1.name, person2.name))
+        return FALSE;
+    if(person1.age != person2.age)
+        return FALSE;
+    if(person1.salary != person2.salary)
+        return FALSE;
+    return TRUE;  
+}
+/*
+1)
+Input person1's name, age, salary :
+hong gil dong 
+40
+400
+Input person2's name, age, salary :
+hong gil dong
+40
+400
+The to human beings are the same
+=======================================
+2)
+Input person1's name, age, salary :
+hong gil dong
+40
+350
+Input person2's name, age, salary :
+kim su mi 
+60
+400
+The to human beings are not the same
+*/
