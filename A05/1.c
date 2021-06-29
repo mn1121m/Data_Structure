ae4e75c097e1a70ff505b058f1388ea147e05b57
mn1121m <jymoon1121@gmail.com>
2021-06-30 00:53:29 +0900

    A05

--- A05/1.c
+++ A05/1.c
@@ -13,20 +13,32 @@ rand()함수를 이용하여 위치와 값을 설정하도록 하시오.
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_TERM_SIZE 401   /* maximum numer of terms + 1 */
+
 #define ROW 20
 #define COL 20
-#define MAX_TERMS 101   /* maximum numer of terms + 1 */
+
+#define TRUE 1
+#define FALSE 0
 
 // Data structure
 
+typedef struct {
+    int row;
+    int col;
+    int value;
+} term;
+
 // Functions
-void createAry();
+void createAry(int n);
 void printAry(int ary[20][20]);
 void ary2SparseMat();
 void printSparseMat();
 
 // Global variable
 int ary[ROW][COL];
+int flag[ROW][COL];     // 중복된값을 판단하기 위해 사용, 기본적으로 false으로 저장됨.
+term mat[MAX_TERM_SIZE];
 
 int main(void)
 {
@@ -35,37 +47,104 @@ int main(void)
     printf("Press the numver of a elements : ");
     scanf("%d", &n);
 
-    createAry();
+    createAry(n);
     printf("Random generated Matrix(20,20)\n");
-    printAry();
+    printAry(ary);
 
     ary2SparseMat();
+    printf("sparse matrix\n");
     printSparseMat();
     return 0;
 }
-void createAry()
+void createAry(int n)
 {
+    int row, col;
+    int i;
 
+    srand(time(NULL));
+
+    for(i = 0; i < n; i ++) {
+        //중복처리
+        row = rand() % ROW;
+        col = rand() % COL;
+        while(flag[row][col]) {    //값이 있으면(TRUE) 다시 뽑으라는 의미이다. flag = FALSE일때 반복문을 빠져나온다.
+            row = rand() % ROW;
+            col = rand() % COL;
+        }
+        flag[row][col] = TRUE;
+        ary[row][col] = rand() % 100 + 1;     //value = rand() % 100 + 1
+    }
 }
-void printAry(int ary[20][20]);
-void ary2SparseMat();
-/* 
-cf) rand() 함수 
-    //srand(time(NULL));
-    
-    // for(i = 1; i <= 10; i++) 
-    //     printf("%d ", (rand() % 100) + 1);
-    // printf("\n");
+void printAry(int ary[20][20])
+{
+    int i, j;
 
+    for(i = 0; i < ROW; i++) {
+        for(j = 0; j < COL; j++) {
+            printf("%3d ", ary[i][j]);
+        }
+        putchar('\n');
+    }
+}
+void ary2SparseMat()
+{
+    int row, col, numValue = 1;
+    int i, j;
 
-힌트)
-for i = 0 to row :
-    for j = 0 to col :
-        if cur.row == i and cur.col == j
-            print(cur.value)
-            cur <- cur_next
-        else
-            print(0)
-    print('\n');
+    row = ROW;
+    col = COL;
 
-*/
+    for(i = 0; i < ROW; i++) {
+        for(j = 0; j < COL; j++) {
+            if(ary[i][j]) {
+                mat[numValue].row = i;
+                mat[numValue].col = j;
+                mat[numValue++].value = ary[i][j];
+            }
+        }
+    }
+    mat[0].row = row;
+    mat[0].col = col;
+    mat[0].value = numValue - 1;
+}
+void printSparseMat()
+{
+    int i;
+
+    for(i = 0; i <= mat[0].value; i++) {
+        printf("%3d,\t%3d\t%3d\n",mat[i].row, mat[i].col, mat[i].value);
+    }
+}
+/*
+Press the numver of a elements : 7
+Random generated Matrix(20,20)
+  0   0   0   0  59   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0  92   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0  16   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   4 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0  39   0   0   0   0   0  99   0   0   0   0 
+  0   0  61   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0 
+sparse matrix
+ 20,     20       7
+  0,      4      59
+  1,     11      92
+  8,     17      16
+  9,     19       4
+ 13,      9      39
+ 13,     15      99
+ 14,      2      61
+ */
--- A05/22.c
+++ A05/22.c
@@ -1,13 +1,8 @@
 /*
-05. 다항식더하기, 전치행렬구하기
+05. 다항식더하기, 전치행렬구하기 - 1번 응용
 
 2. 다음 희소행렬(sparse matrix)에 대한 전치행렬(transpose matrix)을 구하는 함수로 
 Program 2.9 fastTranspose를 구현하여 실행결과를 보여라.
-
-- 추가질문-
-
-문제처럼 "파일 처리" 함수를 이용하여 구현하는 방법에 대해서 잘 모르겠습니다.
-"파일 처리"에 개념은 아는데 개념을 이용해서 풀이하는 방법이 어렵습니다.
 */
 
 #include <stdio.h>
@@ -16,8 +11,8 @@ Program 2.9 fastTranspose를 구현하여 실행결과를 보여라.
 
 #define MAX_TERM_SIZE 401   /* maximum numer of terms + 1 */
 
-#define ROW 6
-#define COL 6
+#define ROW 20
+#define COL 20
 
 #define TRUE 1
 #define FALSE 0
@@ -33,16 +28,13 @@ typedef struct {
 // Functions
 void createAry(int n);
 void ary2SparseMat();
-void printSparseMat(term x[]);
-void printMat(term x[]);
-
-void fast_transpose();
+void printSparseMat();
+void printMat();
 
 // Global variable
 int ary[ROW][COL];
 int flag[ROW][COL];     // 중복된값을 판단하기 위해 사용, 기본적으로 false으로 저장됨.
-term a[MAX_TERM_SIZE];
-term b[MAX_TERM_SIZE];
+term mat[MAX_TERM_SIZE];
 
 int main(void)
 {
@@ -54,14 +46,9 @@ int main(void)
     createAry(n);
     ary2SparseMat();
 
-    printf("A\n");
-    //printSparseMat(a);
-    printMat(a);
-
-    printf("\nB\n");
-    fast_transpose();
-    //printSparseMat(b);
-    printMat(b);
+    printf("sparse matrix\n");
+    printSparseMat();
+    printMat();
     return 0;
 }
 void createAry(int n)
@@ -95,40 +82,41 @@ void ary2SparseMat()
     for(i = 0; i < ROW; i++) {
         for(j = 0; j < COL; j++) {
             if(ary[i][j]) {
-                a[numValue].row = i;
-                a[numValue].col = j;
-                a[numValue++].value = ary[i][j];
+                mat[numValue].row = i;
+                mat[numValue].col = j;
+                mat[numValue++].value = ary[i][j];
             }
         }
     }
-    a[0].row = row;
-    a[0].col = col;
-    a[0].value = numValue - 1;
+    mat[0].row = row;
+    mat[0].col = col;
+    mat[0].value = numValue - 1;
 }
-void printSparseMat(term x[])
+void printSparseMat()
 {
     int i;
 
-    for(i = 0; i <= x[0].value; i++) {
-        printf("%3d,\t%3d\t%3d\n", x[i].row, x[i].col, x[i].value);
+    for(i = 0; i <= mat[0].value; i++) {
+        printf("%3d,\t%3d\t%3d\n",mat[i].row, mat[i].col, mat[i].value);
     }
 }
-void printMat(term x[])
+void printMat()
 {
     int i, j, count = 1;
 
-    for(i = 0; i < x[0].row; i++) {
-        for(j = 0; j < x[0].col; j++)
-            if(i == x[count].row && j == x[count].col)
-                printf("%3d ", x[count++].value);
+    for(i = 0; i < mat[0].row; i++) {
+        for(j = 0; j < mat[0].col; j++)
+            if(i == mat[count].row && j == mat[count].col)
+                printf("%3d ", mat[count++].value);
             else
                 printf("%3d ", 0);
         putchar('\n');
     }
 }
-void fast_transpose()
+/*
+void fast_transpose(term a[],  term b[])
 {
-    int rowTerms[MAX_TERM_SIZE],  startingPos[MAX_TERM_SIZE];
+    int rowTerms[MAX_TERMS],  startingPos[MAX_TERMS];
     int i,j, numCols = a[0].col, numTerms = a[0].value;
     b[0].row = numCols; b[0].col = a[0].row;
     b[0].value = numTerms;
@@ -152,21 +140,4 @@ void fast_transpose()
         }
     }
 }
-/*
-Press the numver of a elements : 8
-A
-  0   0   0   0  90   0 
-  0   0   0   0  48   0 
-  0   0   0   0   0   0 
-  0   0  11  95   0  72 
-  0   0  31  42   0   0 
-  0   0   0   0  72   0 
-
-B
-  0   0   0   0   0   0 
-  0   0   0   0   0   0 
-  0   0   0  11  31   0 
-  0   0   0  95  42   0 
- 90  48   0   0   0  72 
-  0   0   0  72   0   0 
 */
