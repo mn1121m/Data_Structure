176bd0f579246cec2bcd2e2fa7e0c9b7e8ef45ec
mn1121m <83820185+mn1121m@users.noreply.github.com>
2021-09-08 22:27:27 +0900

    updated A01

---
+++ DS-01/2.c
@@ -0,0 +1,21 @@
+/* remind 21_09_08
+다시()
+2. 다음과 같은 자료구조를 생성하는 코드를 작성하고, 배열요소의 역참조를 이용해서 a, b, c의 값을 출력하는 문장을 추가하라.
+단, 변수 a, b, c는 int형이며 포인터 배열을 사용하라
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Functions
+
+// Main
+int main(void)
+{
+    int ary[] = {2, 3, 4};
+    int *a = ary, *b = ary + 1, *c = ary + 2;
+    printf("포인터 배열의 배열요소를 이용한 a, b, c 출력\n");
+
+    printf("a : %d, b : %d, c : %d\n", *a, *b, *c);
+
+    return 0;
+}
