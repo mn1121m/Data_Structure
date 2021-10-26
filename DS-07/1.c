83a2d5af557ce95e6e5240684ffc519c8a110946
mn1121m <83820185+mn1121m@users.noreply.github.com>
2021-10-26 20:56:54 +0900

    remind DS-07

---
+++ DS-07/1.c
@@ -0,0 +1,7 @@
+/* 
+07. 스택과 큐
+1. [ 동적할당배열을 이용한 환형큐(circular queue) ] 
+동적할당 배열을 이용한 환형큐를 생 성하고 실행 예와 같이 수행되는 프로그램을 작성하라. 
+이를 위해, addq, deleteq, queueFull, queueEmpty qprint(queue의 내용을 출력) 함수를 구현하여야 한다. queueFull 함수는 queue capacity를 두 배로 확장한다.
+
+*/
--- DS-07/2.c
+++ DS-07/2.c
@@ -1,1 +1,20 @@
-// 2. mazing problem
+/* pass
+2. Program 3.12 Maze search function을 사용하는 미로탐색 프로그램을 작성하여라. [프로그램 설명]
+다음 1 ~ 5의 변수는 모두 전역으로 선언되며 정적할당을 사용함.
+    1 maze
+    - 입구는 left top, 출구는 right bottom으로 가정
+    - 미로 데이터입력 (“input.txt”, 최대 10×10 행렬로 파일입력)
+    45
+    2 mark 3 move
+    4 stack
+    entrance (1, 1), exit (4, 5)
+    
+    #define MAX_STACK_SIZE 100 typedef struct {
+    short int row; short int col; short int dir;
+    } element;
+    element stack[MAX_STACK_SIZE]; int top = -1;
+    5 기호상수 : 전역변수 :
+    #define TRUE 1
+    #define FALSE 0
+    int EXIT_ROW, EXIT_COL;
+*/
