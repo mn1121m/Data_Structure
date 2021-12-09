141a434e31daab4785ce3a26f8b81974a0676fe4
Junyong Moon <83820185+mn1121m@users.noreply.github.com>
2021-12-09 21:09:17 +0900

    update

--- A-15*/3.c
+++ A-15*/3.c
@@ -1,8 +1,1 @@
-/* pass
-자료구조응용
-15. Binary Search Tree, Winner Tree
-
-3. [승자트리를 이용한 정렬] 10개의 정수형 원소로 구성된 k 개의 run에 대하여 승자트리 (winner tree)를 이용하여 정렬을 수행하라. 
- 이때 k는 2의 누승 ( power of 2 )임을 가정하 라. 단 승자는 키 값이 작은 노드이다.
-
-*/
+//pass
